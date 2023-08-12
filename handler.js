c17f5eeb50712379cb9ea71bd6951eed30831287
ethanp <ethanp.dev@gmail.com>
2023-08-12 14:12:12 +0700

    update serverless

--- handler.js
+++ handler.js
@@ -1,9 +1,25 @@
 "use strict";
 const Res = require("./lamdas/API_Responses");
-const { getUser } = require("./lamdas/getUser");
 
 const hello = async (event) => {
   return Res._200({ message: "Hello World" });
+};
+
+const getUser = async (event) => {
+  if (!event.pathParameters || !event.pathParameters.ID) {
+    // failed without an ID
+    return Res._400({ message: "missing the ID from the path" });
+  }
+
+  let ID = event.pathParameters.ID;
+
+  if (data[ID]) {
+    // return the data
+    return Res._200(data[ID]);
+  }
+
+  // failed as ID not in the data
+  return Res._400({ message: "no ID in data" });
 };
 
 module.exports = {
