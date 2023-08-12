2477f5a6f44e15185865d0c9341316c6fb8327d0
ethanp <ethanp.dev@gmail.com>
2023-08-12 13:54:55 +0700

    Update hello world handler

--- handler.js
+++ handler.js
@@ -1,18 +1,6 @@
-'use strict';
+"use strict";
+const Res = require("./lamdas/API_Responses");
 
 module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  return Res._200({ message: "Hello World" });
 };
