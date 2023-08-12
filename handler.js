e66874fbef6fb011a711359a82b004942f828949
ethanp <ethanp.dev@gmail.com>
2023-08-12 14:17:56 +0700

    add data

--- handler.js
+++ handler.js
@@ -4,7 +4,11 @@ const Res = require("./lamdas/API_Responses");
 const hello = async (event) => {
   return Res._200({ message: "Hello World" });
 };
-
+const data = {
+  1234: { name: "Anna Jones", age: 25, job: "journalist" },
+  7893: { name: "Chris Smith", age: 52, job: "teacher" },
+  5132: { name: "Tom Hague", age: 23, job: "plasterer" },
+};
 const getUser = async (event) => {
   if (!event.pathParameters || !event.pathParameters.ID) {
     // failed without an ID
